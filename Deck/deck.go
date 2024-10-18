a4b0dd5ad7a04fb9d1ca4bf32d22072cb7138936
AlekysCoelho <alekyscoelho@gmail.com>
2024-10-18 11:59:05 -0300

    changes and test implementations for Struct Deck.

--- Deck/deck.go
+++ Deck/deck.go
@@ -1,6 +1,21 @@
 package deck
 
+type DeckInterface interface {
+	AddCards(cards []string)
+	RemoverCard(index int) string
+}
+
 // Represents the deck of cards.
 type Deck struct {
 	Cards []string
+}
+
+func (d *Deck) AddCards(cards []string) {
+	d.Cards = append(d.Cards, cards...)
+}
+
+func (d *Deck) RemoverCard(index int) string {
+	card := d.Cards[index]
+	d.Cards = append(d.Cards[:index], d.Cards[index+1:]...)
+	return card
 }
--- Utils/utils.go
+++ Utils/utils.go
@@ -11,22 +11,37 @@ import (
 // Function to generate the deck of cards.
 // Ranks: Slice of strings with values ​​from 1 to 6
 // Suits: Slice of strings with the suits "♥", "♦", "♣", "♠"
-func GenerateCardsForTheDeck(d *deck.Deck, ranks, suits []string) []string {
-	var card string
-	for _, r := range ranks {
-		for _, s := range suits {
-			card = r + s
-			d.Cards = append(d.Cards, card)
+// func GenerateCardsForTheDeck(d *deck.Deck, ranks, suits []string) []string {
+// 	var card string
+// 	for _, r := range ranks {
+// 		for _, s := range suits {
+// 			card = r + s
+// 			d.Cards = append(d.Cards, card)
+// 		}
+// 	}
+
+// 	return d.Cards
+// }
+
+// Function to generate the deck of cards.
+// Ranks: Slice of strings with values ​​from 1 to 6
+// Suits: Slice of strings with the suits "♥", "♦", "♣", "♠"
+func GenerateCardsForTheDeck(deck deck.DeckInterface, ranks, suits []string) {
+	cards := []string{}
+
+	for _, rank := range ranks {
+		for _, suit := range suits {
+			cards = append(cards, rank+suit)
 		}
 	}
 
-	return d.Cards
+	deck.AddCards(cards)
 }
 
 // Dealing the cards to the player. Function to distribute cards to the player
 // Deck: deck of cards
 // player: Player's hand
-func DealCardsToPlayers(d *deck.Deck, player *player.Player) error {
+func DealCardsToPlayer(d *deck.Deck, player *player.Player) error {
 	if len(d.Cards) == 0 {
 		return errors.New("deck is empty")
 	}
@@ -37,9 +52,11 @@ func DealCardsToPlayers(d *deck.Deck, player *player.Player) error {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i <= 4; i++ {
 		index := r.Intn(len(d.Cards))
-		card := d.Cards[index]
-		player.Hand = append(player.Hand, card)
-		d.Cards = append(d.Cards[:index], d.Cards[index+1:]...)
+		card := d.RemoverCard(index)
+		player.GetCard(card)
+		//card := d.Cards[index]
+		//player.Hand = append(player.Hand, card)
+		//d.Cards = append(d.Cards[:index], d.Cards[index+1:]...)
 	}
 
 	return nil
